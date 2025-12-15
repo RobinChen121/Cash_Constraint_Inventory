2f2c8ba12ed773eefaa4f78c5e28a92d5c0cb9dc
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2025-12-15 23:48:24 +0000

    update

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Created by Zhen Chen on 2025/12/15.
+ * Email: chen.zhen5526@gmail.com
+ * Description: 
+ *
+ *
+ */
+
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::vector prices = {1.2, 2.0};
+    std::vector unit_order_costs = {1.0, 1.5};
+    double rate = 0.0;
+    std::vector unit_salvage_value = {0.5, 0.75};
+
+    std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
